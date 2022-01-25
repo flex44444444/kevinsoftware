4b1af019b473ac413a662ae966c3ff4592f4130c
flex44444444 <flex44444444@gmail.com>
2022-01-25 17:37:52 -0500

    big spicy chungus man

--- QC.js
+++ QC.js
@@ -1,14 +1,41 @@
-const QCBTN = document.querySelector(".QCBTN");
-const QCMenu = document.querySelector(".QCMenu");
+const QCBTN = document.querySelector('#QCBTN')
+const QCMenu = document.querySelector('.QCMenu')
+let currentQCBTNRotation = -90
 
-QCBTN.addEventListener("click", () => {
+//toggle using QC button
+QCBTN.addEventListener('click', () => {
+  QCBTN.setAttribute(
+    'style',
+    `transform: rotate(${currentQCBTNRotation + 180}deg)`,
+  )
+  currentQCBTNRotation += 180
 
-    if(QCMenu.classList.contains("QCMenu-open")) {
-        QCMenu.classList.remove("QCMenu-open");
-        QCMenu.classList.add("QCMenu-closed");
-    } else {
-        QCMenu.classList.add("QCMenu-open");
-        QCMenu.classList.remove("QCMenu-closed");
+  if (QCMenu.classList.contains('QCMenu-open')) {
+    QCMenu.classList.remove('QCMenu-open')
+    QCMenu.classList.add('QCMenu-closed')
+  } else {
+    QCMenu.classList.add('QCMenu-open')
+    QCMenu.classList.remove('QCMenu-closed')
+  }
+})
+
+//force close when clicking anywhere off the menu
+document.addEventListener('click', (e) => {
+  if (
+    !(
+      e.target.classList.contains('QCMenu') ||
+      e.target == QCBTN ||
+      e.target.closest('.QCMenu')
+    )
+  ) {
+    if (QCMenu.classList.contains('QCMenu-open')) {
+      QCBTN.setAttribute(
+        'style',
+        `transform: rotate(${currentQCBTNRotation + 180}deg)`,
+      )
+      currentQCBTNRotation += 180
     }
-
+    QCMenu.classList.remove('QCMenu-open')
+    QCMenu.classList.add('QCMenu-closed')
+  }
 })
